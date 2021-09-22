785b7ae958c373d9d0fbc1b9ffcca4615abaed31
DavidJMartin <davidjmartin@hotmail.com>
2021-09-22 21:09:27 +0100

    homepage styling

--- cot-report-frontend/frontend/src/app/cot-report/cot-report.component.ts
+++ cot-report-frontend/frontend/src/app/cot-report/cot-report.component.ts
@@ -14,83 +14,102 @@ export class CotReportComponent implements OnInit {
   defaultSelection: string = 'USD';
   tableData: any;
   chartData: any;
+  // lineChartData: any;
   seriesData: any;
   showSpinner = true;
   showLogoutButton = false;
   view: [number, number] = [600, 250];
   // dtOptions: DataTables.Settings = {};
 
+  lineChartData: any = {
+    data: [
+      {
+        name: 'longs',
+        series: []
+      },
+      {
+        name: 'shorts',
+        series: []
+      }
+    ]
+  };
+
+  // lineChart options
+  // showXAxis = true;
+  // showYAxis = true;
+  // gradient = false;
+  // showLegend = true;
+  // showXAxisLabel = true;
+  // xAxisLabel = 'Dates';
+  // showYAxisLabel = true;
+  // yAxisLabel = 'Prices';
+  // autoScale = true
+
   constructor(private http: HttpClient, private router: Router) {
     console.log('GET http://cot.com/api/reports/cot?symbol=' + this.defaultSelection)
     this.http.get(`http://cot.com/api/reports/cot?symbol=` + this.defaultSelection).subscribe((data: any) => {      
       // this.showLogoutButton = true;
-
-      this.tableData = data;
- 
+      this.tableData = data; 
       this.chartData = [        
         { name: "shorts", value: this.tableData[0].shortPositions },
         { name: "longs", value: this.tableData[0].longPositions },
       ];
-
       setTimeout(() => {
-        $('#datatableexample').DataTable({
-          "bPaginate": true,
-          "bLengthChange": true,
-          "lengthMenu": [6, 10, 20],
-          "bFilter": false,
-          "bInfo": true,
-          "bAutoWidth": false,
-          "ordering": false,
-          "fixedHeader": false,
-          "iDisplayLength": 6,
-          "pageLength": 6
-        });
+        this.buildDataTable();
       }, 10);
 
+      console.log(data);
+
+      data.forEach((report: { reportDate: any; longPositions: any; }) => {
+        this.lineChartData.data[0].series.push({ name: report.reportDate, value: [report.longPositions]});        
+      });
+     
       this.showSpinner = false;
     }, (error: any) => console.error(error));
   }
 
   ngOnInit(): void {
-
   }
   
   getSymbolData(theSymbol: string) {
     $('#datatableexample').hide();
     console.log('GET http://cot.com/api/reports/cot?symbol=' + theSymbol)
     this.http.get('http://cot.com/api/reports/cot?symbol=' + theSymbol).subscribe((data: any) => {
-
-    // update tableData   
- 
-
+        
+    // update table data
     $('#datatableexample').DataTable().destroy();
     this.tableData = data;
-
     setTimeout(() => {
-      $('#datatableexample').DataTable({
-        "bPaginate": true,
-        "bLengthChange": true,
-        "lengthMenu": [6, 10, 20],
-        "bFilter": false,
-        "bInfo": true,
-        "bAutoWidth": false,
-        "ordering": false,
-        "fixedHeader": false,
-        "iDisplayLength": 6,
-        "pageLength": 6
-      });
+      this.buildDataTable();
     }, 1);
-
     this.showSpinner = false;
     $('#datatableexample').show();
 
-    // update chart data     
-      this.chartData = [
+    // update pie chart data      
+    this.chartData = [
         { name: "shorts", value: this.tableData[0].shortPositions },
         { name: "longs", value: this.tableData[0].longPositions }
       ];
     }, (error: any) => console.error(error));    
 
+    // update line chart data
+
+
+  }
+
+  private buildDataTable() {
+    $('#datatableexample').DataTable({
+      "bPaginate": true,
+      "bLengthChange": true,
+      "lengthMenu": [6, 10, 20],
+      "bFilter": false,
+      "bInfo": true,
+      "bAutoWidth": false,
+      "ordering": false,
+      "fixedHeader": false,
+      "iDisplayLength": 6,
+      "pageLength": 6
+    });
   }
 
   handleChange(theSymbol: string) {    
