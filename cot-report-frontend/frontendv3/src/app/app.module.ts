76d9cb44d12fc0d77ae477d9fcbd1ae8bb050993
DavidJMartin <davidjmartin@hotmail.com>
2021-12-26 22:03:04 +0000

    build v2 frontend - added charts and table

--- cot-report-frontend/frontendv3/src/app/app.module.ts
+++ cot-report-frontend/frontendv3/src/app/app.module.ts
@@ -11,7 +11,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
-import { NgChartsModule } from 'ng2-charts';
+import { ChartsModule } from 'ng2-charts';
 import { ReportComponent } from './report/report.component';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
@@ -20,13 +20,22 @@ import { CardComponent } from './card/card.component';
 import { MatRadioModule } from '@angular/material/radio';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { LineChartComponent } from './charts/line-chart/line-chart.component';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatInputModule } from '@angular/material/input';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSortModule } from '@angular/material/sort';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     ReportComponent,
-    CardComponent    
+    CardComponent, 
+    LineChartComponent   
   ],
   imports: [
     BrowserModule,
@@ -38,13 +47,19 @@ import { HttpClientModule } from '@angular/common/http';
     MatSidenavModule,
     MatIconModule,
     MatListModule,
-    NgChartsModule,
+    ChartsModule,
     MatGridListModule,
     MatCardModule,
     MatMenuModule,
     MatRadioModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    NgxChartsModule,
+    MatInputModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatProgressSpinnerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- cot-report-frontend/frontendv3/src/app/report/report.component.ts
+++ cot-report-frontend/frontendv3/src/app/report/report.component.ts
@@ -10,12 +10,16 @@ import { HttpClient } from '@angular/common/http';
 })
 export class ReportComponent {
 
+  // chart data
+  pieChartData: any;  
+  changeChartSize: [number, number] = [130, 180];
   
   // dataList = new MatTableDataSource();
+  data: any;
+
+  // radio-button group 
   selectedSymbol: string = 'USD';
   symbols: string[] = ['USD', 'EUR', 'GBP', 'AUD', 'NZD', 'JPY', 'CHF', 'CAD', 'CAD', 'MXN', 'ZAR', 'BTC'];
-
-
 
   /** Based on the screen size, switch from standard to one column per row */
   cardLayout = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
@@ -25,34 +29,110 @@ export class ReportComponent {
           columns: 1,
           miniCard: { cols: 1, rows: 1 },
           chart: { cols: 1, rows: 2 },
-          table: { cols: 1, rows: 4 },
+          table: { cols: 1, rows: 1 },
         };
       }
  
      return {
-        columns: 4,
-        miniCard: { cols: 4, rows: 1 },
+        columns: 3,
+        miniCard: { cols: 3, rows: 1 },
         chart: { cols: 1, rows: 3 },
-        table: { cols: 4, rows: 5 },
+        table: { cols: 3, rows: 5 },
       };
     })
   );
 
-  constructor(private breakpointObserver: BreakpointObserver, private http: HttpClient) {}
+  constructor(private breakpointObserver: BreakpointObserver, private http: HttpClient) {
+    this.getSymbolData(this.selectedSymbol);
+  }
 
-  handleChange(theSymbol: string) {
+  handleChange(theSymbol: string) {    
     this.getSymbolData(theSymbol);
   }
 
+    multi: any[] | undefined;
+    lineChartSize: [number, number] = [330, 210];
+    // line chart options
+    showXAxis = false;
+    showYAxis = true;
+    gradient = true;
+    showLegend = false;
+    xAxisLabel = 'Time';
+    showYAxisLabel = true;
+    showXAxisLabel = true;
+    yAxisLabel = 'Institutional Positions';
+    autoScale = true;
+    legendTitle = 'Positions';
+    changeLabel = '% change'
   
-  getSymbolData(symbol: string) {
+  
+  getSymbolData(symbol: string) {    
     this.http
       .get<any>('http://my-example.com:80/api/reports/cot?symbol=' + symbol)
       .subscribe((response) => {
-        console.log('GET: http://localhost:80/api/reports/cot?symbol=USD');
-        // this.dataList.data = response.slice(0, 11);
+        console.log('GET: http://my-example.com:80/api/reports/cot?symbol=' + symbol);
+        this.data = response.slice(0, 6);
+        this.buildPieChartData(this.data);        
+        this.buildLineChartData(this.data);
+        this.buildChangeData(this.data);
       }, error => {
         console.log(error);
       })
   }
+
+  positionChangeData: any;
+  buildChangeData(data: any) {
+    this.positionChangeData = [
+      {
+        "name": "% change ",
+        "series": [
+          {
+            "name": "sells",
+            "value": Number(data[0].percentageShortChange)
+          },
+          {
+            "name": "buys",
+            "value": Number(data[0].percentageLongChange)
+          }
+        ]
+      }
+    ]
+  }
+
+  private buildPieChartData(data: any) {
+    this.pieChartData = [
+      { name: "shorts", value: data[0].shortPositions },
+      { name: "longs", value: data[0].longPositions },
+    ];
+  }
+
+  
+  // line chart
+  private buildLineChartData(data: any) {
+    this.multi = [    
+      {
+        "name": "Open Positions",
+        "series": this.getOpenPositions(data)
+      }
+    ];
+  }
+
+  getOpenPositions(data: any) {
+    const array: { name: any; value: any; }[] = [];
+    data.forEach((report: { reportDate: any; netPositions: any; }) => {
+      array.push({ name: report.reportDate, value: report.netPositions });
+    });
+    return array.reverse();
+  }
+
+  displayedColumns =
+  [
+    'reportDate',
+    'longPositions',
+    'shortPositions',
+    'percentageLong',
+    'percentageShort',
+    'netPositions'
+  ];
+
 }
