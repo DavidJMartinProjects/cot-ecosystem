843457d13eb57d6d60d11cc732a0f64867dabd65
DavidJMartin <davidjmartin@hotmail.com>
2021-12-26 17:45:05 +0000

    frontend v3 layour behaviour

--- cot-report-frontend/frontendv3/src/app/app-routing.module.ts
+++ cot-report-frontend/frontendv3/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NavComponent } from './nav/nav.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // { path: '', component: NavComponent }
+  // { path: 'home', component: NavComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
