16cd31960d584de32d8091512632ccd5ade61269
DavidJMartin <davidjmartin@hotmail.com>
2021-12-27 04:01:58 +0000

    refactorings

--- cot-report-service/src/main/java/com/cot/app/backend/scheduled/ScheduledTask.java
+++ cot-report-service/src/main/java/com/cot/app/backend/scheduled/ScheduledTask.java
@@ -2,6 +2,7 @@ package com.cot.app.backend.scheduled;
 
 import com.cot.app.backend.scheduled.utils.DataUtil;
 import com.cot.app.backend.scheduled.utils.ExcelFileUtils;
+import com.cot.app.backend.scheduled.utils.ZipFileUtils;
 import com.cot.app.backend.swap.SwapsWebScraper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,27 +24,28 @@ public class ScheduledTask {
     private ReportDownloader reportDownloader;
 
     @Autowired
-    private DataUtil dataProcessorUtil;
+    private DataUtil dataUtil;
 
     @Autowired
     private SwapsWebScraper swapsWebScraper;
 
     public void importReport() throws IOException {
-//        log.info("retrieving latest cot report");
-//        reportDownloader.downloadFile();
-//
-//        log.info("unzipping cot report data");
-//        FileUtils.unzip();
-//
-//        log.info("saving Report To Db");
-//        excelFileUtils.saveReportToDb();
-//
-//        log.info("processing data...");
-//        dataProcessor.processData();
-//
-////        log.info("cleaning work directory files.");
-////        org.apache.commons.io.FileUtils.cleanDirectory(new File(REPORT_DOWNLOAD_LOCATION));
-//        log.info("cot-report retrieved successfully.");
+        log.info("retrieving latest cot report");
+        reportDownloader.downloadFile();
+
+        log.info("unzipping cot report data");
+        ZipFileUtils.unzip();
+
+        log.info("saving Report To Db");
+        excelFileUtils.saveReportToDb();
+
+
+        log.info("processing data...");
+        dataUtil.process();
+
+//        log.info("cleaning work directory files.");
+//        org.apache.commons.io.FileUtils.cleanDirectory(new File(REPORT_DOWNLOAD_LOCATION));
+        log.info("cot-report retrieved successfully.");
 
         log.info("retrieving latest swap data.");
         swapsWebScraper.scrape();
