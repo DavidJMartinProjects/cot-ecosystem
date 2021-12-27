f264f837a7eff472b97502b17bf1a94d646ee582
DavidJMartin <davidjmartin@hotmail.com>
2021-12-27 20:59:22 +0000

    refactorings

--- cot-report-api/src/main/java/com/cot/app/backend/scheduled/ScheduledTask.java
+++ cot-report-api/src/main/java/com/cot/app/backend/scheduled/ScheduledTask.java
@@ -3,7 +3,6 @@ package com.cot.app.backend.scheduled;
 import com.cot.app.backend.scheduled.utils.DataUtil;
 import com.cot.app.backend.scheduled.utils.ExcelFileUtils;
 import com.cot.app.backend.scheduled.utils.ZipFileUtils;
-import com.cot.app.backend.swap.SwapsWebScraper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -26,30 +25,15 @@ public class ScheduledTask {
     @Autowired
     private DataUtil dataUtil;
 
-    @Autowired
-    private SwapsWebScraper swapsWebScraper;
-
     public void importReport() throws IOException {
         log.info("retrieving latest cot report");
         reportDownloader.downloadFile();
-
-        log.info("unzipping cot report data");
         ZipFileUtils.unzip();
-
-        log.info("saving Report To Db");
         excelFileUtils.saveReportToDb();
-
-
-        log.info("processing data...");
         dataUtil.process();
-
 //        log.info("cleaning work directory files.");
 //        org.apache.commons.io.FileUtils.cleanDirectory(new File(REPORT_DOWNLOAD_LOCATION));
         log.info("cot-report retrieved successfully.");
-
-        log.info("retrieving latest swap data.");
-        swapsWebScraper.scrape();
-        log.info("swap data retrieved successfully.");
     }
 
 }
