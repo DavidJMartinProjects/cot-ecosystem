f978e7f13ab7db117c667d420554ef74281a6ff9
DavidJMartin <davidjmartin@hotmail.com>
2021-12-28 16:33:50 +0000

    added skaffold and helm charts

--- api-gateway/src/main/java/com/cot/app/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/cot/app/ApiGatewayApplication.java
@@ -1,13 +1,17 @@
 package com.cot.app;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class ApiGatewayApplication {
 
     public static void main(String[] args) {
+        log.info("api-gateway initialising...");
         SpringApplication.run(ApiGatewayApplication.class, args);
+
     }
 
 }
