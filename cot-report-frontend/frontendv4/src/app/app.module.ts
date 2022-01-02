454074837cda698268e2cd2108e0933d11a3f4a5
unknown <davidjmartin@hotmail.com>
2022-01-02 15:54:58 +0000

    added frontendv4 mockup

---
+++ cot-report-frontend/frontendv4/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CotTableComponent } from './components/cot-table/cot-table.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CotTableComponent,
+    NavbarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
