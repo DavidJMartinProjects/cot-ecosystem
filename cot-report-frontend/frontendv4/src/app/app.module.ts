c7758a7b3db16c0c8c80d558679885dd2b9ffa68
unknown <davidjmartin@hotmail.com>
2022-01-02 17:27:49 +0000

    adding main layout for frontend

--- cot-report-frontend/frontendv4/src/app/app.module.ts
+++ cot-report-frontend/frontendv4/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CotTableComponent } from './components/cot-table/cot-table.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { MainContentComponent } from './components/main-content/main-content.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CotTableComponent,
-    NavbarComponent
+    NavbarComponent,
+    MainContentComponent
   ],
   imports: [
     BrowserModule,
