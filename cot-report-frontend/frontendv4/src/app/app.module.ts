77396a5d5ca99f6c72930029cc519a3ff2df2e0f
unknown <davidjmartin@hotmail.com>
2022-01-02 19:56:37 +0000

    added sidebar

--- cot-report-frontend/frontendv4/src/app/app.module.ts
+++ cot-report-frontend/frontendv4/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { CotTableComponent } from './components/cot-table/cot-table.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { MainContentComponent } from './components/main-content/main-content.component';
+import { SideBarComponent } from './components/side-bar/side-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CotTableComponent,
     NavbarComponent,
-    MainContentComponent
+    MainContentComponent,
+    SideBarComponent
   ],
   imports: [
     BrowserModule,
