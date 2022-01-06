17cbfbad5b80957058110e4c344b1d905e11ccb9
DavidJMartin <davidjmartin@hotmail.com>
2022-01-06 18:25:14 +0000

    added charts

--- cot-report-frontend/frontendv5/src/app/components/cot-table/cot-table.component.ts
+++ cot-report-frontend/frontendv5/src/app/components/cot-table/cot-table.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartType } from 'angular-google-charts';
 import { CotReportService } from 'src/app/services/cot-report-backend.service';
 
 
@@ -9,12 +10,21 @@ import { CotReportService } from 'src/app/services/cot-report-backend.service';
 })
 export class CotTableComponent implements OnInit {
   
+  pieChartData: any;
   data: any;
 
   constructor(private cotReportService: CotReportService) {}
 
   ngOnInit(): void {
     this.cotReportService.dataSource.subscribe(data => this.data = data)
+    this.cotReportService.dataSource.subscribe(data => this.pieChartData = this.buildPieChartData(data)) 
+  }
+
+  buildPieChartData(data: any) {
+    return this.pieChartData = [
+      [ "shorts", data[0].shortPositions ],
+      [ "longs", data[0].longPositions ],
+    ];
   }
 
   displayedColumns =
@@ -26,5 +36,26 @@ export class CotTableComponent implements OnInit {
     'percentageShort',
     'netPositions'
   ];
+
+  myType = ChartType.PieChart;
+  width = 160;
+  height = 160;
+  options = {
+    legend: { position: 'bottom' },      
+    colors: ['#949FB1', 'green'],
+    pieHole:0.8,
+    animation: {      
+      duration: 2000,
+      easing: 'in',
+      startup: true
+   },
+   pieSliceTextStyle: {
+    color: 'white',    
+  }
+  };
+
+  myCountUpOpts = {
+    duration: .3
+  }
 
 }
