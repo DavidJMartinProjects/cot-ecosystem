7d510c2e85b3ae8b862c14e8ce794e3ce48a181c
DavidJMartin <davidjmartin@hotmail.com>
2022-01-15 20:29:19 +0000

    sidebar now toggles

--- cot-report-frontend/frontendv6/src/app/components/side-bar/side-bar.component.ts
+++ cot-report-frontend/frontendv6/src/app/components/side-bar/side-bar.component.ts
@@ -7,15 +7,17 @@ import { SideNavService } from 'src/app/services/side-bar.service';
   styleUrls: ['./side-bar.component.scss']
 })
 export class SideBarComponent implements OnInit {
-
-  constructor(private sideNavService: SideNavService) { }
-
+  
+  isSideMenuEnabled: boolean = true;
+  
   ngOnInit(): void {
+    this.sideNavService.isMenuShowing.subscribe(isMenuEnabled => this.isSideMenuEnabled = isMenuEnabled);
   }
 
+  constructor(private sideNavService: SideNavService) { }
   
   toggle() {
-    // console.log("this.navbarComponent.togglemenu(): " + this.sideNavService.toggle());
+    console.log("isSideMenuEnabled" + this.isSideMenuEnabled);
     return this.sideNavService.toggle();
   } 
 
