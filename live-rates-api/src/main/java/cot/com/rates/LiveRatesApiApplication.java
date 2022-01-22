9bea927f4ff4e9f6016bda87fe75724a32f3bf0c
DavidJMartin <davidjmartin@hotmail.com>
2022-01-22 13:51:10 +0000

    added live-rates-api service

---
+++ live-rates-api/src/main/java/cot/com/rates/LiveRatesApiApplication.java
@@ -0,0 +1,13 @@
+package cot.com.rates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveRatesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiveRatesApiApplication.class, args);
+	}
+
+}
---
+++ live-rates-api/src/test/java/cot/com/rates/LiveRatesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package cot.com.rates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveRatesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
