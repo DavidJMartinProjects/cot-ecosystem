b1b52454b4095a042a9ed6d05aed186266bf0a26
DavidJMartin <davidjmartin@hotmail.com>
2022-01-23 12:00:15 +0000

    add websocket to connect to external live rates feed service

---
+++ live-rates-api/src/main/java/cot/com/rates/App.java
@@ -0,0 +1,20 @@
+package cot.com.rates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ *
+ * @author Daniel
+ */
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+}
+
+
